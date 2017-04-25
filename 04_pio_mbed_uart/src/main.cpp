070a85ecc6650a03b71db929f0b8e50ddf02b7dc
wassfila <wassim.filali@gmail.com>
2017-04-25 20:17:10 +0200

    added multi uarts and reception

--- 04_pio_mbed_uart/src/main.cpp
+++ 04_pio_mbed_uart/src/main.cpp
@@ -1,17 +1,23 @@
 #include "mbed.h"
 
 DigitalOut  myled(PC_13);
-Serial      pc(PB_10, PB_11, 115200);
+Serial      pc_log(PB_10, PB_11, 115200);
+Serial      pc_Control(PA_2, PA_3, 115200);
 
+void uart_callback() 
+{
+    pc_Control.putc(pc_Control.getc());//echo the character
+}
 
 int main() 
 {
     int count = 0;
-    pc.printf("Hello from the World of the STM32 Blue pill\r\n");
+    pc_Control.attach(&uart_callback);
+    pc_log.printf("Hello from the World of the STM32 Blue pill\r\n");
     while(1) 
     {
         count++;
-        pc.printf("log cycle:%d\r\n",count);
+        pc_log.printf("log cycle:%d\r\n",count);
         myled = 1; // LED is ON
         wait(1.0); // 1 sec
         myled = 0; // LED is OFF
