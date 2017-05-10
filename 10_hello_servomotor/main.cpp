aa3acedd89600c6b9f29d490b3d6959a38a6a0e8
wassfila <wassim.filali@gmail.com>
2017-05-10 20:21:39 +0200

    simpler example with lib servo class only

--- 10_hello_servomotor/main.cpp
+++ 10_hello_servomotor/main.cpp
@@ -3,44 +3,17 @@
 
 DigitalOut myled(PC_13);
 
-class ServoOut: public PwmOut
-{
-    public:
-        ServoOut(PinName pin) : PwmOut(pin)
-        {
-            PwmOut::period_ms(20);
-            offset = 1.5;
-            period = 20;
-            range = 1;//+- 1 ms
-        }
-    public:
-        void setpos(float pos)
-        {
-            float pos_us = ((offset + pos * range) * 1000);
-            PwmOut::pulsewidth_us(pos_us);
-        }
-    public:
-        float period;
-        float offset;
-        float range;
-};
-
-ServoOut servo(PB_4);
-
-Servo servo2(PA_11);
+Servo servo(PA_11);
 
 int main() 
 {
     while(1) 
     {
-        servo.setpos(1);
-        servo2 = 0;
-        //servo.pulsewidth_us(500);
+        servo = 0;
         myled = 1; // LED is ON
         wait(1.0);
-        servo.setpos(-1);
-        servo2 = 1;
-        //servo.pulsewidth_us(2500);
+
+        servo = 1;
         myled = 0; // LED is OFF
         wait(1.0); // 1 sec
     }
