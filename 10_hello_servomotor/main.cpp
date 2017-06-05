ca3aae51edd63863a88b53bb5932a64a57336143
wassfila <wassim.filali@gmail.com>
2017-06-05 20:23:53 +0200

    moving ignores to local dirs and adding some eclipse files

--- 10_hello_servomotor/main.cpp
+++ 10_hello_servomotor/main.cpp
@@ -3,17 +3,29 @@
 
 DigitalOut myled(PC_13);
 
-Servo servo(PA_11);
+Serial   rasp(PB_10, PB_11, 115200);
+
+Servo servo0(PA_8);
+Servo servo1(PB_0);
+Servo servo2(PA_2);
+Servo servo3(PA_3);
 
 int main() 
 {
+    rasp.printf("Hello\n");
     while(1) 
     {
-        servo = 0;
+        servo0 = 0;
+        servo1 = 0;
+        servo2 = 0;
+        servo3 = 0;
         myled = 1; // LED is ON
         wait(1.0);
 
-        servo = 1;
+        servo0 = 1;
+        servo1 = 1;
+        servo2 = 1;
+        servo3 = 1;
         myled = 0; // LED is OFF
         wait(1.0); // 1 sec
     }
