b235aebcdfa0c49fc2a190793d1a1e59394416d8
wassfila <wassim.filali@gmail.com>
2017-06-17 20:12:26 +0200

    removed unused dependencies as found a read working lib

--- 19_Hello_TimeOfFlight/main.cpp
+++ 19_Hello_TimeOfFlight/main.cpp
@@ -1,49 +1,53 @@
 #include "mbed.h"
 
-#define SENSOR_LIB 1
-
-#ifdef SENSOR_LIB
-#include "vl53l0x_class.h"
-#endif
-
-/* This VL53L0X Expansion board test application performs a range measurement in polling mode
-   on the onboard embedded top sensor. */
- 
-#define VL53L0_I2C_SDA   PB_7 
-#define VL53L0_I2C_SCL   PB_6 
 
 Serial   sp(PB_10, PB_11, 115200);
 DigitalOut pintest(PB_13);
 
+#define MBEDOS_READYLIB 1
 
-#ifdef DIRECT_TEST
-I2C i2c(VL53L0_I2C_SDA,VL53L0_I2C_SCL);
+#ifdef MY_LIB_REMAKE
+    #include "vl53l0x.h"
+    vl53l0x sensor(&sp,PB_7,PB_6);//SDA, SCL
 #endif
 
-#ifdef SENSOR_LIB
-DevI2C device_i2c(VL53L0_I2C_SDA, VL53L0_I2C_SCL);
-DigitalOut shutdown(PB_1);
-VL53L0X sensor(device_i2c,shutdown,PB_13,DEFAULT_DEVICE_ADDRESS);//Shutdown Not connected PB_1, Int on PB13 - signal
+#ifdef MBEDOS_READYLIB
+#include "VL53L0X.h"
+I2C i2c(PB_7,PB_6);//SDA, SCL
+Timer t;
+VL53L0X sensor(&i2c,&t);
 #endif
-/*=================================== Main ==================================
-=============================================================================*/
+
 int main()
 {
     sp.printf("Hello Time Of Flight\n");
     uint32_t distance;
     int  status;
    
-#ifdef DIRECT_TEST
-    i2c.frequency(400000);
-    wait_ms(10);
+#ifdef MBEDOS_READYLIB
+    sp.printf("Init Sensor()\n");
     pintest = 1;
-   char subAddress = 0xC0;
-    char data = 0xFF;
-    i2c.write(0x52|0x00, &subAddress, 1, 1); // no stop
-    i2c.read(0x52|0x01, &data, 1, 0);
-    wait_ms(10);
+    status = sensor.init();
     pintest = 0;
-    sp.printf("read from 0xC0 : 0x%x\n",data);
+
+    while(true)
+    {
+        wait_ms(1000);
+        uint16_t distance_mm = sensor.readRangeSingleMillimeters();
+        sp.printf("distance is : %u\n",distance_mm);
+    }
+#endif
+
+
+#ifdef MY_LIB_REMAKE
+    pintest = 1;
+    if(sensor.isPresent() )
+    {
+        pintest = 0;
+        sp.printf("ToF> I'm here\n");
+        sensor.init();
+    }
+    pintest = 0;
     while(1)
     {
         wait_ms(1000);
